3b317bb8ce484e0eee067abee181a4492745cd82
DNS <37093+DNS@users.noreply.github.com>
2025-07-31 02:25:02 +0700

    start over

--- Solution.java
+++ Solution.java
@@ -10,15 +10,43 @@ class Solution {
 		// create 1 board
 		char[] b = new char[n];
 		Arrays.fill(b, '.');
-		String s = new String(b);
+		String s1 = new String(b);
 		for (int i=0; i<n; i++) {
-			l.add(s);
+			l.add(s1);
 		}
-		System.out.println(l);
+		//System.out.println(l);
 		
-		// try put queen
+		
+		// IsValid
+		// check horizontal
+		l.set(0, "...Q");
 		for (int i=0; i<n; i++) {
-			
+			//System.out.println( l.get(i).contains("Q") );
+			// if (! l.get(i).contains("Q") )
+		}
+		
+		// check vertical
+		for (int i=0; i<n; i++) {
+			String s2 = "";
+			for (int j=0; j<n; j++) {
+				//s2 += new String(l.get(i)[j]);
+				//System.out.println( l.get(i).substring(j,j+1) );
+				//if ( l.get(i).charAt(j) == 'Q' )
+					//System.out.println( l.get(i).charAt(j) );
+			}
+			//System.out.println(s2);
+		}
+		
+		// check diagonally
+		// linear gradient formula: m = (y2-y1) / (x2-x1)
+		// top-left to bottom-light: 1
+		// top-right to bottom-left: -1
+		for (int i=0; i<n; i++) {
+			if (i == x) continue;
+			for (int j=0; j<n; j++) {
+				int m = (j-y) / (i-x);
+				if (l.get(i).charAt(j) == 'Q' && (m == 1 || m == -1)) return false;
+			}
 		}
 		
 		
