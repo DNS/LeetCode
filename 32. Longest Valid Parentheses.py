ed629fde934e1ac4f3932adfb48c7cfa776c87c9
DNS <37093+DNS@users.noreply.github.com>
2025-10-18 04:43:54 +0700

    -

--- 32. Longest Valid Parentheses.py
+++ 32. Longest Valid Parentheses.py
@@ -2,23 +2,65 @@
 class Solution:
 	def longestValidParentheses (self, s: str) -> int:
 		self.count = 0
-		self.a = 0
+		i = 0
 		self.s = s
 		self.solve()
 		return self.count
 		
 
 	def solve (self):
-		while self.a < len(self.s)-1 :
-			if self.s[self.a:self.a+2] == '()':
-				self.s = self.s[0:self.a] + self.s[self.a+2:len(self.s)]
-				self.count += 2
-				self.a = 0
+		i = 0
+		while i < len(self.s)-1 :
+			# find () pattern
+			if self.s[i:i+2] == '()':
+				self.s = substr_replace(self.s, '..', i, i+2)
+				i = 0
+				#self.count += 2
+				#print('a')
+			
 			else:
-				self.a += 1
+				i += 1
+		
+		i = 0
+		while i < len(self.s)-1 :
+			# find (...) pattern
+			if self.s[i:i+1] == '(':
+				j = i + 1
+				print('b')
+				while j < len(self.s)-1 :
+					#print('c')
+					if self.s[j:j+1] == '.':
+						print('d')
+						j += 1
+					elif self.s[j:j+1] == ')' :
+						self.s = substr_replace(self.s, '.', i, i+1)
+						self.s = substr_replace(self.s, '.', j, j+1)
+						i = 0
+						print('e')
+						break
+					elif self.s[j:j+1] == '(' :
+						#i = 0
+						print('f: ' + str(j))
+						break
+					#print('z')
+			else:
+				i += 1
+		
+
+# substr_replace(self.s, '..', i, i+2)
+def substr_replace (string_input: str, string_replace: str, start: int, end: int) -> str:
+	return string_input[:start] + string_replace + string_input[end:]
+
+
+
 
 text = '())()()((()()()))()()()()()()())))(()(())))()())()()()())(((()))))()()())()()()()())))('
 s = Solution()
 s.longestValidParentheses(text)
 print( s.count )
+print( s.s )
+
+#())()()((()()()))()()()()()()())))(()(())))()())()()()())(((()))))()()())()()()()())))(
+#())()()((()()()))()()()()()()())))(()(())))()())()()()())(((()))))()()())()()()()())))(
+#..)....((......))..............)))(..(..)))....)........)(((..))))......)..........)))(
 
