0c71e29d4c34334fd2107037989aa5adfda1ba1e
DNS <37093+DNS@users.noreply.github.com>
2025-10-23 01:01:48 +0700

    30 -

--- 30. Substring with Concatenation of All Words.py
+++ 30. Substring with Concatenation of All Words.py
@@ -5,46 +5,63 @@ class Solution:
 	def __init__ (self):
 		self.retval = []
 		self.words = []
-		
+		self.joined = ''
+		self.words_len = 0
+	
 	def findSubstring (self, s: str, words: List[str]) -> List[int]:
 		self.s = s
 		
 		
 		self.words = words
+		self.words_len = len(self.words[0])
 		
-		
-		self.permute(self.words, 0, len(self.words) - 1 )
+		self.permute(self.words, 0, self.words_len - 1 )
 		
 		# remove duplicates
 		seen = set()
 		self.retval = [x for x in self.retval if not (x in seen or seen.add(x))]
 		return self.retval
-
+	
 	def swap (self, arr, a, b) :
 		arr[a], arr[b] = arr[b], arr[a]
-
+	
 	def permute (self, arr, left, right) :
 		#print(arr)
+		#print(2)
+		j = ''.join(arr)
+		if j == self.joined:
+			return
+			
 		if left == right:
-			joined = ''.join(arr)
-			#print(joined)
-			self.findIndex(joined)
+			self.joined = ''.join(arr)
+			#print(self.joined)
+			self.findIndex()
 		else:
 			for i in range(left, right + 1) :
 				self.swap(arr, left, i)
 				self.permute(arr, left + 1, right)
-				self.swap(arr, left, i)  # backtrack
+				self.swap(arr, left, i)		# backtrack
 	
 	
-	def findIndex (self, joined) :
-		joined_length = len(joined)
+	def findIndex (self) :
+		joined_length = len(self.joined)
+		#'''
 		for i in range(len(self.s)-joined_length+1) :
 			#print(self.s[i:i+joined_length])
-			if self.s[i:i+joined_length] == joined:
+			if self.s[i:i+joined_length] == self.joined:
 				#print(i)
 				self.retval.append(i)
-				
-		
+		#'''
+		'''
+		i = 0
+		right = len(self.s)-joined_length+1
+		while True:
+			if self.s[i:i+joined_length] == self.joined:
+				self.retval.append(i)
+			if i >= right:
+				break
+			i += self.words_len
+		'''
 
 
 
@@ -57,6 +74,7 @@ if __name__ == "__main__":
 	#text = "wordgoodgoodgoodbestword"
 	#words = ["word","good","best","word"]
 	
+	# error
 	#text = "barfoothefoobarman"
 	#words = ["foo","bar"]
 	
@@ -66,10 +84,31 @@ if __name__ == "__main__":
 	#text = "wordgoodgoodgoodbestword"
 	#words = ["word","good","best","good"]
 	
-	text = "fffffffffffffffffffffffffffffffff"
-	words = ["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"]
+	#text = "fffffffffffffffffffffffffffffffff"
+	#words = ["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"]
+	
+	#text = "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel"
+	#words = ["dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"]
 	
 	s = Solution()
 	s.findSubstring(text, words)
 	print(s.retval)
 
+dhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcb
+
+pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmy
+xgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoa
+dfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjyctt
+prkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwa
+jrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhduce
+sctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtrax
+rlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabe
+jifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtq
+kscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyr
+dtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkp
+uuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjh
+zadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrow
+zljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisr
+dhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcb
+
+zvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel
