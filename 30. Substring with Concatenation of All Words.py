6622f0444738e00cdf15c99ac792cbb741598214
DNS <37093+DNS@users.noreply.github.com>
2025-10-22 16:54:07 +0700

    30 time limit exceeded

--- 30. Substring with Concatenation of All Words.py
+++ 30. Substring with Concatenation of All Words.py
@@ -4,34 +4,44 @@ from typing import *
 class Solution:
 	def __init__ (self):
 		self.retval = []
+		self.words = []
 		
 	def findSubstring (self, s: str, words: List[str]) -> List[int]:
 		self.s = s
+		
+		
 		self.words = words
+		
+		
 		self.permute(self.words, 0, len(self.words) - 1 )
 		
+		# remove duplicates
+		seen = set()
+		self.retval = [x for x in self.retval if not (x in seen or seen.add(x))]
 		return self.retval
 
 	def swap (self, arr, a, b) :
 		arr[a], arr[b] = arr[b], arr[a]
 
 	def permute (self, arr, left, right) :
+		#print(arr)
 		if left == right:
 			joined = ''.join(arr)
+			#print(joined)
 			self.findIndex(joined)
-			
 		else:
 			for i in range(left, right + 1) :
 				self.swap(arr, left, i)
 				self.permute(arr, left + 1, right)
 				self.swap(arr, left, i)  # backtrack
-				
+	
+	
 	def findIndex (self, joined) :
 		joined_length = len(joined)
-		for i in range(len(self.s)-joined_length) :
+		for i in range(len(self.s)-joined_length+1) :
 			#print(self.s[i:i+joined_length])
 			if self.s[i:i+joined_length] == joined:
-				print(i)
+				#print(i)
 				self.retval.append(i)
 				
 		
@@ -53,8 +63,11 @@ if __name__ == "__main__":
 	#text = "barfoofoobarthefoobarman"
 	#words = ["bar","foo","the"]
 	
-	text = "wordgoodgoodgoodbestword"
-	words = ["word","good","best","good"]
+	#text = "wordgoodgoodgoodbestword"
+	#words = ["word","good","best","good"]
+	
+	text = "fffffffffffffffffffffffffffffffff"
+	words = ["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"]
 	
 	s = Solution()
 	s.findSubstring(text, words)
